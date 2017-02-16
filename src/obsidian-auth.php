9996e9deaf27bbf00442081f1471331049df58dd
SamLangTen <samlangten@outlook.com>
2017-02-16 13:27:47 +0800

    add plugin startup

---
+++ src/obsidian-auth.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Obsidian Authentication Plugin
+Plugin URI:  https://github.com/ZA-PT/Obsidian.Authentication.WordPress
+Description: A WordPress plugin provided a convenient to access Obsidian-based Authentication Server.
+Version:     0.0.1
+Author:      ZA-PT
+Author URI:  http://www.za-pt.org
+License:     Apache License 2.0
+License URI: http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+?>
