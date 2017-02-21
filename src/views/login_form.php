97255ccc41d02b86ab0d376c08caba1a8ef4c2cf
SamLangTen <samlangten@outlook.com>
2017-02-21 21:30:57 +0800

    improve ui in login form

--- src/views/login_form.php
+++ src/views/login_form.php
@@ -15,7 +15,7 @@
         if(($server->grant_mode=="token")||($server->grant_mode=="code"))
         {
         ?>
-            <p><a class="button button-primary button-large" href="<?php echo(home_url()."/obsidian-auth/auth?server_name=".$server->server_name."&action=login");?>" style="margin-bottom:16px;float:none;"><?php printf(__("Login with %s","obsidian-auth"),$server->server_name); ?></a></p>
+            <p><a class="button button-primary button-large" href="<?php echo(home_url()."/obsidian-auth/auth?server_name=".$server->server_name."&action=login");?>" style="margin-bottom:16px;float:none;width:100%;text-align:center;"><?php printf(__("Login with %s","obsidian-auth"),$server->server_name); ?></a></p>
         <?php
         }
     }
