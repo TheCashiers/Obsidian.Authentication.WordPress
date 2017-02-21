a3998aea7ca658d766cf766a0965baf2e66bc528
SamLangTen <samlangten@outlook.com>
2017-02-21 21:07:04 +0800

    add server selection in login

--- src/hook-handler.php
+++ src/hook-handler.php
@@ -2,6 +2,7 @@
     require_once(ROOT_PATH."/oauth-controller.php");
     require_once(ROOT_PATH."/views/view-controller.php");
     require_once(ROOT_PATH."/authentication/resource-owner-password-credential-authentication.php");
+    require_once(ROOT_PATH."/options/server-option.php");
     class obsidian_hook_handler
     {
         /* Handler for Installation*/
@@ -32,60 +33,47 @@
             //get all Obsidian-based servers from database;
             $servers = json_decode(get_option("obsidian_servers"));
             if($servers == null) return $user;
-            foreach ($servers as $value) {
-                //if server grant is not password mode
-                if(($value->grant_mode)!="password") continue;
-                //no input
-                if($username==""&&$password=="") return $user;
-                //intercept
-                if(!is_wp_error($user)&&$value->password_mode_intercept=="no") return $user;
-                //authenticate in a server
-                $client = new obsidian_client($value->client_id,$value->client_secret);
-                $password_auth = new resource_owner_password_credential_authentication($value->password_mode_request_url,$client);
-                $result = $password_auth->authenticate($username,$password,explode(" ",$value->scope_login));
-                //check result
-                if($result==false||!isset($password_auth->access_token))
-                    if($value->password_mode_intercept=="no")
-                        continue;
-                    else
-                        return null;
-                //decode and get userinfo
-                $jwt = json_web_token::decode_jwt($password_auth->access_token);
-                $token_id = $jwt->custom_claims["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
-                $token_username = $jwt->custom_claims["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"];
-                $token_email = $jwt->custom_claims["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"];
-                //login to WordPress
-                $users = get_users(array("meta_key"=>"obsidian_server_binding_id_".$value->server_name,"meta_value"=>$token_id));
-                //if user exist,login
-                if(count($users)>0)
-                    return $users[0];
-                elseif($value->allow_login_unbind_user_pasword_mode=="yes") //if there is a user with a same name in Obsidian server
+            //if "obsidian_auth_server" was posted, use specified server.Or use the first password mode server in the database
+            //when there is no other password mode server or obsidian_disable_internal_auth is "yes",login with WordPress Internal Authentication
+            if(($_POST["obsidian_auth_server"]=="wp_internal")&&(get_option("obsidian_disable_internal_auth")=="no")) return $user;
+            $server = server_option::get_server_by_name($_POST["obsidian_auth_server"]);
+  
+            if($server==null) foreach($servers as $value) if($value->grant_mode=="password"){$server=$value; break;}
+            if($server==null) return $user;
+            //authenticate in a server
+            $client = new obsidian_client($server->client_id,$server->client_secret);
+            $password_auth = new resource_owner_password_credential_authentication($server->password_mode_request_url,$client);
+            $result = $password_auth->authenticate($username,$password,explode(" ",$server->scope_login));
+            if($result==false||!isset($password_auth->access_token)) return null;
+            //decode and get userinfo
+            $jwt = json_web_token::decode_jwt($password_auth->access_token);
+            $token_id = $jwt->custom_claims["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+            $token_username = $jwt->custom_claims["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"];
+            $token_email = $jwt->custom_claims["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"];
+            //login to WordPress
+            $users = get_users(array("meta_key"=>"obsidian_server_binding_id_".$server->server_name,"meta_value"=>$token_id));
+            //if user exist,login
+            if(count($users)>0)
+                return $users[0];
+            elseif($server->allow_login_unbind_user_pasword_mode=="yes") //if there is a user with a same name in Obsidian server
+            {
+                $user_login = get_user_by("login",$token_username);
+                //if allow insert user
+                if(($user_login==null)&&($server->allow_create_user=="yes"))
                 {
-                    $user_login = get_user_by("login",$token_username);
-                    //if allow insert user
-                    if(($user_login==null)&&($value->allow_create_user=="yes"))
-                    {
-                        $userdata = array(
-                            "user_login" => str_replace(" ","",$value->server_name)."_".$token_username,
-                            "user_pass"  => md5_file(rand().$token_id)."_obsidian",
-                            "user_email" => $token_email,
-                            "user_nicename" => $token_username,
-                            "display_name" => $token_username
-                            );
-                        $user_login =get_user_by("id",wp_insert_user($userdata));
-                    }
-                    if($user_login!=null)
-                        update_user_meta($user_login->ID,"obsidian_server_binding_id_".$value->server_name,$token_id);
-                    return $user_login;
+                    $userdata = array(
+                        "user_login" => str_replace(" ","",$server->server_name)."_".$token_username,
+                        "user_pass"  => md5_file(rand().$token_id)."_obsidian",
+                        "user_email" => $token_email,
+                        "user_nicename" => $token_username,
+                        "display_name" => $token_username
+                        );
+                    $user_login =get_user_by("id",wp_insert_user($userdata));
                 }
-                else
-                    if($value->password_mode_intercept=="no")
-                        continue;
-                    else
-                        return null;
-            }
-            //if no server,use WordPress internal login process
-            return $user;
+                if($user_login!=null)
+                    update_user_meta($user_login->ID,"obsidian_server_binding_id_".$server->server_name,$token_id);
+                return $user_login;
+            }            
         }
 
         /*Called when user browser a url*/
--- src/views/login_form.php
+++ src/views/login_form.php
@@ -1,5 +1,15 @@
 <?php          
     $servers = json_decode(get_option("obsidian_servers"));
+    //for password mode,create combobox
+    $o_html = "<p><label for=\"obsidian_auth_server\">".__("Authentication Server","obsidian-auth")."<br/><select class=\"input\" name=\"obsidian_auth_server\" id=\"obsidian_auth_server\">";
+    $op_html = "";
+    $owp_html = "";
+    foreach($servers as $server)
+        if($server->grant_mode=="password")
+            $op_html.="<option value=\"".$server->server_name."\">".$server->server_name."</option>";
+    if(get_option("obsidian_disable_internal_auth")=="no") $owp_html = "<option value=\"wp_internal\">".__("WordPress Internal Authentication","obsidian-auth")."</option>";
+    if($op_html!="") echo($o_html.$owp_html.$op_html."</select></label></p>");
+    //for token or code mode,show buttons bottom
     foreach($servers as $server)
     {
         if(($server->grant_mode=="token")||($server->grant_mode=="code"))
