954f63a2cd02cf9cb3e50d91e9a0bde02136c21c
SamLangTen <samlangten@outlook.com>
2017-03-02 12:54:42 +0800

    change file name to correct name

--- src/views/view-controller.php
+++ src/views/view-controller.php
@@ -3,23 +3,23 @@
     {
         public static function client_administrator_list_servers()
         {
-            include_once(ROOT_PATH."/views/client_administrator_list_servers.php");
+            include_once(ROOT_PATH."/views/client-administrator-list-servers.php");
         }
         public static function client_administrator_add_server()
         {
-            include_once(ROOT_PATH."/views/client_administrator_add_server.php");
+            include_once(ROOT_PATH."/views/client-administrator-add-server.php");
         }
         public static function client_administrator_option()
         {
-            include_once(ROOT_PATH."/views/client_administrator_option.php");
+            include_once(ROOT_PATH."/views/client-administrator-option.php");
         }
         public static function login_form()
         {
-            include_once(ROOT_PATH."/views/login_form.php");
+            include_once(ROOT_PATH."/views/login-form.php");
         }
         public static function edit_user_profile()
         {
-            include_once(ROOT_PATH."/views/edit_user_profile.php");
+            include_once(ROOT_PATH."/views/edit-user-profile.php");
         }
     }
 ?>
